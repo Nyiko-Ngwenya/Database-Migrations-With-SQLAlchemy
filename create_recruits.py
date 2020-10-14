c1115ad32c49e14e0a199ff24773d41663a43c0d
Nyiko <nyikongwenya@gmail.com>
2020-10-14 12:36:36 +0200

    Added personal email address information in create recruits

--- create_recruits.py
+++ create_recruits.py
@@ -8,8 +8,8 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 session.add_all([
-     Recruit(first_name='Wendy', surname='Williams', chatname='windy',github_name='windy@github',id_number=123456789),
-     Recruit(first_name='Mary', surname='Contrary', chatname='mary',github_name='mary@github',id_number=134679258),
-     Recruit(first_name='Fred', surname='Flintstone', chatname='freddy',github_name='freddy@github',id_number=159258357)])
+     Recruit(first_name='Wendy', surname='Williams', chatname='windy',github_name='windy@github',id_number=123456789,personal_email_address='windy@gmail.com'),
+     Recruit(first_name='Mary', surname='Contrary', chatname='mary',github_name='mary@github',id_number=134679258,personal_email_address='windy@yahoo.com'),
+     Recruit(first_name='Fred', surname='Flintstone', chatname='freddy',github_name='freddy@github',id_number=159258357,personal_email_address='windy@personal.com')])
 
 session.commit()
